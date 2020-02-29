474019bf84339bec94995e2418df4bba310cf6e3
Mike Jouwstra <mike.jouwstra@gmail.com>
2020-02-29 00:05:31 -0700

    updated to allow bundling of multiple groups

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 var pjson = require('./package.json');
 var fs = require('fs');
-if(pjson.assetBundler && pjson.assetBundler.vendorPaths) {
-    var vendorPathLen = Object.keys(pjson.assetBundler.vendorPaths).length;
-    if(vendorPathLen > 0) {
-        var outputFile = pjson.assetBundler.output;
-        fs.writeFile(outputFile,"",function(err, result) {
-            if (err) throw err;
-        });
-        Object.keys(pjson.dependencies).forEach(key => {
-            let value = pjson.dependencies[key];
-            var data = {key:key, val:value, path:"./node_modules/" + key};
-            fs.stat(data.path, function(err, stats) {
-                data.isDirectory = stats.isDirectory();
-                if(data.isDirectory && pjson.assetBundler.vendorPaths[key] !== undefined) {
-                    data.file = pjson.assetBundler.vendorPaths[key];
-                    var vendorPath = data.path + "/" + data.file;
-                    fs.readFile(vendorPath, (err, content) => {
-                        if (err) throw err;
+
+var stat = fs.stat;
+var assetBundler = pjson.assetBundler;
+
+exports.bundleAssets = function() {
+    if(assetBundler && assetBundler.bundles) {
+        if(assetBundler.bundles.length > 0) {
+            assetBundler.bundles.forEach(function(bundle, idx) {
+                var outputFile = bundle.output;
+                fs.writeFile(outputFile,"",function(err, result) {
+                    if (err) throw err;
+                });
+                bundle.files.forEach(function(filepath, x) {
+                    fs.readFile(bundle.basePath + filepath, (err, content) => {
+                        if(err) throw err;
+                        content += "\n";
                         fs.appendFile(outputFile, content, function(err, result) {
-                            if (err) throw err;
-                            console.log(key + " written to " + outputFile);
+                            if(err) throw err;
                         });
                     });
-                }
+                });
             });
-        });
+        }
     }
 }
+
+//exports.bundleAssets();
--- readme.md
+++ readme.md
@@ -1,20 +1,39 @@
-#assetbundler
+# asset bundler
 ---
 easy to use bundler of asset files.
 
-Define the order files are built within the vendorPaths config object.
+Set the shared path of your files in bundle.basePath.
+Define the order of builds within the bundle.files array.
+Define the path to combine your files into with the bundle.output property.
 
-Define the file to combine your files into with assetBundler.output property.
-
-This is a sample snippet to add to your package.json file:
+This is a sample snippet which belongs in your package.json file:
 
 ```
 "assetBundler": {
-    "vendorPaths": { 
-        "angular": "angular.min.js",
-        "@uirouter/angularjs": "release/angular-ui-router.min.js",
-        "angular-material": "angular-material.min.js"
-    },
-    "output": "./public/js/vendor.js"
+    "bundles": [
+        {
+            "basePath": "./node_modules/",
+            "files": [
+                "angular/angular.min.js",
+                "@uirouter/angularjs/release/angular-ui-router.min.js",
+                "angular-material/angular-material.min.js"
+            ], 
+            "output": "./public/js/vendor.js"
+        },{
+            "basePath": "./assets/js/",
+            "files": [
+                "app.js",
+                "controllers/MainController.js",
+                "services/API.js"
+            ], 
+            "output": "./public/js/scripts.js"
+        },{
+            "basePath": "./assets/css/",
+            "files": [
+                "style.css"
+            ], 
+            "output": "./public/css/style.css"
+        }
+    ]
 }
 ```
